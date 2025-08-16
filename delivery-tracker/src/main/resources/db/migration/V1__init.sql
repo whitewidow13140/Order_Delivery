7808d05c8fe9153f357ec5c6c84cfa1ef5fa302e
whitewidow13140 <alexis.doumax@epitech.eu>
2025-08-16 14:12:04 +0200

    Update V1__init.sql

--- delivery-tracker/src/main/resources/db/migration/V1__init.sql
+++ delivery-tracker/src/main/resources/db/migration/V1__init.sql
@@ -1,9 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS delivery_tracker;
 
 CREATE TABLE IF NOT EXISTS delivery_tracker.deliveries (
-  id        BIGSERIAL PRIMARY KEY,
-  order_id  BIGINT NOT NULL,
-  item      VARCHAR(255) NOT NULL,
-  quantity  INTEGER NOT NULL,
-  status    VARCHAR(32)
+  id          BIGSERIAL PRIMARY KEY,
+  order_id    BIGINT       NOT NULL,
+  item        VARCHAR(255) NOT NULL,
+  quantity    INTEGER      NOT NULL,
+  status      VARCHAR(32)  NOT NULL,
+  created_at  TIMESTAMPTZ  NOT NULL,
+  updated_at  TIMESTAMPTZ  NOT NULL
 );
